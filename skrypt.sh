9071eb3a72be9a857decbf56dc54f1c15a579eac
Jędrzej <jedrek.kozina02@gmail.com>
2023-04-06 00:20:09 +0200

    Dodano funkcję Log

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,24 @@
+
 Pomoc()
 {
 	echo "Oto flagi przygotowane wtym skrypcie! "
-	echo "-h Otwiera pomoc.\n"
-	echo "-d Pokazuje dzisiejszą datę.\n"
+	echo "-h Otwiera pomoc."
+	echo "-d Pokazuje dzisiejszą datę."
+	echo "-l <number> Tworzy plik log, number wskazuje ile razy utworzy plik."
 }
+Log()
+{
+	mkdir logs
+	cd logs
+	for (( i=1; i<=arg_x ; i++))
+	do
+	touch log$i.txt
 
+	echo "plik został utworzony przez skrypt: skrypt_lox.sh\n Date: " >> log$i.txt 
+	date >> log$i.txt
+
+	done
+}
 Date()
 {
 	echo "Dzisiejsza data\n"
@@ -12,9 +26,9 @@ Date()
 	exit 0
 }
 
-while getopts ":help:date" flag
+while getopts 'hdl:' flag;
 do
-	case "${flag}" in
+	case $flag in
 	h)
 		Pomoc
 		exit 0
@@ -25,6 +39,9 @@ do
 		exit 0
 	;;
 	
-	
+	l)
+		arg_x=$((OPTARG))
+		Log 
+	;;
 	esac
 done
