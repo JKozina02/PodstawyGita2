b49dc040b3fc8ef3782f40d329bfb95721d6084d
Jędrzej <jedrek.kozina02@gmail.com>
2023-04-06 16:28:07 +0200

    Nowa funkcja init oraz długie flagi

--- skrypt.sh
+++ skrypt.sh
@@ -1,47 +1,69 @@
-
 Pomoc()
 {
 	echo "Oto flagi przygotowane wtym skrypcie! "
-	echo "-h Otwiera pomoc."
-	echo "-d Pokazuje dzisiejszą datę."
-	echo "-l <number> Tworzy plik log, number wskazuje ile razy utworzy plik."
+	echo "-h|--help Otwiera pomoc."
+	echo "-d|--date Pokazuje dzisiejszą datę."
+	echo "-l <number>|--log <number> Tworzy plik log, number wskazuje ile razy utworzy plik."
 }
 Log()
 {
 	mkdir logs
 	cd logs
-	for (( i=1; i<=arg_x ; i++))
+	i=$1
+	while ((i>0)) 
 	do
-	touch log$i.txt
-
-	echo "plik został utworzony przez skrypt: skrypt_lox.sh\n Date: " >> log$i.txt 
-	date >> log$i.txt
-
+		touch log$i.txt
+		echo "plik został utworzony przez skrypt: skrypt.sh" >> log$i.txt 
+		date >> log$i.txt
+		i=$(($i-1))
 	done
+	
 }
 Date()
 {
-	echo "Dzisiejsza data\n"
+	echo "Dzisiejsza data:"
 	date
 	exit 0
 }
 
-while getopts 'hdl:' flag;
+if ! flag=$(getopt -o hdl:i -l help,date,log:,init -- $@)
+then
+	exit 1
+fi
+set -- $flag
+
+while [ $# -gt 0 ]
 do
-	case $flag in
-	h)
+	case $1 in
+	
+	-h|--help)
 		Pomoc
-		exit 0
 	;;
 	
-	d)
+	-d|--date)
 		Date
-		exit 0
 	;;
 	
-	l)
-		arg_x=$((OPTARG))
-		Log 
+	-l|--log)
+		argx=$2
+		a=${argx:1:-1}
+		
+		Log $a
+		shift
+	;;
+	
+	-i|--init)
+		rep="https://github.com/JKozina02/PodstawyGita2"
+		fol="PodstawyGita2"
+		dir=$(pwd)
+		
+		git clone $rep $fol
+		echo "sklonowano repozytorium"
+		
+		echo "export PATH=\"$dir/$fol:\$PATH\"" >> ~/.bashrc
+		source ~/.bashrc
+		echo "utworzono ścierzkę PATH"
 	;;
 	esac
+	exit 0 
 done
