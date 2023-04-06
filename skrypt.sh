549ee5fd71d5c9671ba49a7f771f4dd944379eba
Jędrzej <jedrek.kozina02@gmail.com>
2023-04-06 17:41:56 +0200

    Dodano skrypt error oraz zaktualizowano help

--- skrypt.sh
+++ skrypt.sh
@@ -3,8 +3,9 @@ Pomoc()
 	echo "Oto flagi przygotowane wtym skrypcie! "
 	echo "-h|--help Otwiera pomoc."
 	echo "-d|--date Pokazuje dzisiejszą datę."
-	echo "-l <number>|--log <number> Tworzy plik log, number wskazuje ile razy utworzy plik."
+	echo "-l <number>|--log <number> Tworzy plik log w katalogu log, number wskazuje ile razy utworzy plik."
 	echo "-i|--init klonuje repozytorium oraz tworzy ścieżkę PATH"
+	echo "-e <number>|--error <number> Tworzy plik error w katalogu error, number wskazuje ile razy utworzy plik."
 }
 Log()
 {
@@ -13,10 +14,25 @@ Log()
 	i=$1
 	while ((i>0)) 
 	do
+		mkdir log"$i"
 		touch log$i.txt
-		echo "plik został utworzony przez skrypt: skrypt.sh" >> log$i.txt 
-		date >> log$i.txt
+		echo "plik został utworzony przez skrypt: skrypt.sh" >> log$i/log$i.txt 
+		date >> log$i/log$i.txt
 		i=$(($i-1))
+	done
+	
+}
+Error()
+{
+	mkdir error
+	cd error
+	y=$1
+	while ((y>0)) 
+	do
+		mkdir error"$y"
+		touch error$y.txt
+		echo "plik został utworzony przez skrypt: skrypt.sh" >> error$y/error$y.txt 
+		y=$(($y-1))
 	done
 	
 }
@@ -27,7 +43,7 @@ Date()
 	exit 0
 }
 
-if ! flag=$(getopt -o hdl:i -l help,date,log:,init -- $@)
+if ! flag=$(getopt -o hdl:ie: -l help,date,log:,init,error: -- $@)
 then
 	exit 1
 fi
@@ -64,6 +80,14 @@ do
 		source ~/.bashrc
 		echo "utworzono ścierzkę PATH"
 	;;
+	
+	-e|--error)
+		argy=$2
+		n=${argy:1:-1}		
+		Error $n
+		shift
+	;;
 	esac
+	
 	exit 0 
 done
